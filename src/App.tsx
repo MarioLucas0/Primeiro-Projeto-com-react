4e79c698d5238ef522d212eb9a0ee309e00d4a2c
Lucas <mariolucas.2000@outlook.com>
2022-07-15 21:34:54 -0300

    Navbar stylling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import './assets/styles/custom.scss'
 import './App.css'
 
-function App() {
-  return <h1 className="bg-primary">Hello DSCatalog</h1>
+function Navbar() {
+  return (
+    <>
+      <nav className="bg-primary">
+        <div>
+          <a href="link">
+            <h4>DS Catalog</h4>
+          </a>
+          <div>
+            <ul>
+              <li>
+                <a href="link">HOME</a>
+              </li>
+              <li>
+                <a href="link">Catalago</a>
+              </li>
+              <li>
+                <a href="link">ADMIN</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>{' '}
+    </>
+  )
 }
 
-export default App
+export default Navbar
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
-import App from './App'
+import Navbar from './components/Navbar/navbar'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>
 )
